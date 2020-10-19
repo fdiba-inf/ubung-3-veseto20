8a226aef4af9b851db416c8a3d0a6ee7d88fa1e2
veseto20 <vesela_krumova2001@abv.bg>
2020-10-19 07:28:28 +0000

    for,while...

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -6,21 +6,27 @@ public class NumberCounter {
   public static void main (String[] args) {
 
     Scanner input = new Scanner(System.in);
-    double a;
-    double b;
-    double c;
-    do {
-      a=input.nextDouble();
-      b=input.nextDouble();
-      c=input.nextDouble();
-    } while (a>0 && b>0 && c>0);
-    System.out.println("Positive numbers:");
-  } if (a<=0 && b<=0 && c<=0);
-    System.out.println("Negative numbers:");
-} double sum = a+b+c;
- System.out.println("Sum:" + sum);
- double average = (a+b+c)/3;
-  System.out.println("Average:" + average);
+    int positive = 0;
+        int negative = 0;
+        double sum = 0;
+        int average = 0;
+        int count = 0;
+        int number= input.nextInt();
+        while ( number !=0 ){
+          if (number>0){
+            positive++;
+          } else {
+            negative++;
+          }
+          count++;
+          sum = sum + number;
+          number= input.nextInt();
+        }
+        System.out.println("Positive numbers: " + positive);
+        System.out.println("Negative numbers: " + negative);
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + sum/count);
+      
     
 
   }
